93bbaa1e15f4701229f84b691afb7688b0a7157a
Shaina.T <151636181+Telenshaina@users.noreply.github.com>
2025-02-05 01:41:40 +0000

    act1

---
+++ act1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#ACTIVITY 1 - TELEN, SHAINA BLESSY  FEB 05 2025
+
+#Declare a variable
+NAME = "Name"
+AGE = "Age"
+PROGRAM = "Program"
+
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+#Ask for user input
+echo "Enter your name: "
+read NAME
+echo "Enter your age: "
+read AGE
+echo "Enter your program: "
+read PROGRAM
+
+#Display
+echo "Hello, $NAME! This is your age: $AGE"
+echo "& Your program is $PROGRAM"
---
+++ sample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Declare a variable
+USER_NAME = "Student"
+
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+#Ask for user input
+echo "Enter your name: "
+read USER_NAME
+
+#Display
+echo "Nice to meet you, $USER_NAME!"
